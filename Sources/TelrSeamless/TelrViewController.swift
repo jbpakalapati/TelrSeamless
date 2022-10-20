042e36995288e0cdd069d2e4b282ce73be344fc9
jbpakalapati <jyothibabuios@gmail.com>
2022-10-20 15:49:43 +0400

    removed nscodera

--- Sources/TelrSeamless/TelrViewController.swift
+++ Sources/TelrSeamless/TelrViewController.swift
@@ -85,10 +85,6 @@ public class TelrViewController: UIViewController, XMLParserDelegate, UICollecti
     
  
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
