b9a066b553f7f750e4754eb2af60f0e0b7b59282
jbpakalapati <jyothibabuios@gmail.com>
2022-10-20 17:11:12 +0400

    added init for struct

--- Sources/TelrSeamless/trxData.swift
+++ Sources/TelrSeamless/trxData.swift
@@ -9,6 +9,8 @@ import Foundation
 
 
 public struct trxData{
+    
+    
     public var custTitle : String
     public var custFirstName : String
     public var custLastName : String
@@ -26,7 +28,31 @@ public struct trxData{
     public var trxAmount : Int
     public var test : Int
     public var custref : String
+    
+    public init(custTitle: String, custFirstName : String, custLastName : String, addLine: String, addCity: String, addRegion : String, addCountry : String, email : String, IPaddrress : String, storeID : String, authKey : String, cartID : String, cartDesc : String, currency : String, trxAmount : Int, test : Int, custref : String) {
+        
+        self.custTitle = custTitle
+        self.custFirstName = custFirstName
+        self.custLastName = custLastName
+        self.addLine = addLine
+        self.addCity = addCity
+        self.addRegion = addRegion
+        self.addCountry = addCountry
+        self.email = email
+        self.IPaddrress = IPaddrress
+        self.storeID = storeID
+        self.authKey = authKey
+        self.cartID = cartID
+        self.cartDesc = cartDesc
+        self.currency = currency
+        self.trxAmount = trxAmount
+        self.test = test
+        self.custref = custref
+        
+        }
 }
+
+
 
 
 
